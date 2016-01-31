cbbf3fd41a50df6db882bca6ae62bc6094b430eb
Vincent QB <vincentqb@users.noreply.github.com>
2016-01-30 20:31:21 -0500

    Updating variables.

--- predict_new.py
+++ predict_new.py
@@ -7,7 +7,7 @@ from time import clock
 
 train_file = 'dataset/numerai_training_data.csv'
 test_file = 'dataset/numerai_tournament_data.csv'
-output_file = 'predict.csv'
+predict_file = 'predict.csv'
 
 start = clock()
 train = pd.read_csv(train_file)
@@ -15,9 +15,8 @@ test = pd.read_csv(test_file)
 print('Loaded {:d} train and {:d} entries in {:.0f} seconds.'.format( 
     len(train), len(test), clock() - start))
 
-# Validation set is more reprensentative of tournament data
-# train.drop('validation', axis = 1 , inplace = True)
-train = train.drop(train[train.validation == 1].index)
+# No need for validation flag for final training and extrapolation
+train.drop('validation', axis = 1 , inplace = True)
 
 ### One-hot encode of categorical variable
 
@@ -26,40 +25,30 @@ assert(set(train['c1'].unique()) == set(test['c1'].unique()))
 
 # Encode train column, then drop original column
 train_dummies = pd.get_dummies(train['c1'])
-train_num = pd.concat((train.drop('c1', axis = 1), train_dummies.astype(int)), axis = 1)
+train = pd.concat((train.drop('c1', axis = 1), train_dummies.astype(int)), axis = 1)
 
 # Encode test column, then drop original column
 test_dummies = pd.get_dummies(test['c1'])
-test_num = pd.concat((test.drop('c1', axis = 1), test_dummies.astype(int)), axis = 1)
+test = pd.concat((test.drop('c1', axis = 1), test_dummies.astype(int)), axis = 1)
 
 ### Select classifier
 
 from sklearn.ensemble import RandomForestClassifier as rf
-n_trees = 1000
-clf = rf(n_estimators = n_trees, verbose = True)
-
-### Cross validation
-
-from sklearn.cross_validation import cross_val_score
-
-start = clock()
-scores = cross_val_score(clf, train, label)
-print("Performed {:d}-fold cross validation in {:.0f} seconds with accuracy {:0.4f} +/- {:0.4f}.".format(
-    len(scores), clock() - start, scores.mean(), scores.std() ))
+clf = rf(n_estimators = 1000, verbose = True)
 
 ### Fit training data
 
 start = clock()
-clf.fit(train_num.drop('target', axis = 1), train_num.target)
-print("Fitted training data in {:.0f} seconds.".format(clock() - start))
+clf.fit(train.drop('target', axis = 1), train.target)
+print("Fitted in {:.0f} seconds.".format(clock() - start))
 
-### Extrapolate to test data
+### Extrapolate
 
 start = clock()
-predict = clf.predict_proba(test_num.drop('t_id', axis = 1))
-print("Extrapolated to test data in {:.0f} seconds.".format(clock() - start))
+predict = clf.predict_proba(test.drop('t_id', axis = 1))
+print("Extrapolated in {:.0f} seconds.".format(clock() - start))
 
 ### Save results
 
-test_num['probability'] = predict[:,1]
-test_num.to_csv(output_file, columns = ('t_id', 'probability'), index = None)
+test['probability'] = predict[:,1]
+test.to_csv(predict_file, columns = ('t_id', 'probability'), index = None)
