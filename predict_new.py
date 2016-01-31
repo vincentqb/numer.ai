2b3e77342088cbd0642d426ec2f717d56e9b1c56
Vincent QB <vincentqb@users.noreply.github.com>
2016-01-31 13:13:31 -0500

    updating style.

--- predict_new.py
+++ predict_new.py
@@ -18,37 +18,41 @@ print('Loaded {:d} train and {:d} test entries in {:.0f} seconds.'.format(
 # No need for validation flag for final training and extrapolation
 train.drop('validation', axis = 1 , inplace = True)
 
+# Separate data and target label
+train_target = train_data['target']
+train_data.drop('target', axis = 1, inplace = True)
+
 ### One-hot encode of categorical variable
 
 # Check train and test have the same categories
-assert(set(train['c1'].unique()) == set(test['c1'].unique()))
+assert(set(train_data['c1'].unique()) == set(test_data['c1'].unique()))
 
-# Encode train column, then drop original column
-train_dummies = pd.get_dummies(train['c1'])
-train = pd.concat((train.drop('c1', axis = 1), train_dummies.astype(int)), axis = 1)
+# Encode column in train, then drop original column
+train_dummies = pd.get_dummies(train_data['c1'])
+train_data = pd.concat((train_data.drop('c1', axis = 1), train_dummies.astype(int)), axis = 1)
 
-# Encode test column, then drop original column
-test_dummies = pd.get_dummies(test['c1'])
-test = pd.concat((test.drop('c1', axis = 1), test_dummies.astype(int)), axis = 1)
+# Encode column in test, then drop original column
+test_dummies = pd.get_dummies(test_data['c1'])
+test_data = pd.concat((test_data.drop('c1', axis = 1), test_dummies.astype(int)), axis = 1)
 
 ### Select classifier
 
-from sklearn.ensemble import RandomForestClassifier as rf
+from sklearn.ensemble import RandomForestClassifier as RF
 clf = rf(n_estimators = 1000, verbose = True)
 
 ### Fit training data
 
 start = clock()
-clf.fit(train.drop('target', axis = 1), train.target)
+clf.fit(train_data, train_target)
 print("Fitted in {:.0f} seconds.".format(clock() - start))
 
 ### Extrapolate
 
 start = clock()
-predict = clf.predict_proba(test.drop('t_id', axis = 1))
+predict = clf.predict_proba(test_data)
 print("Extrapolated in {:.0f} seconds.".format(clock() - start))
 
 ### Save results
 
-test['probability'] = predict[:,1]
-test.to_csv(predict_file, columns = ('t_id', 'probability'), index = None)
+test_data['probability'] = predict[:,1]
+test_data.to_csv(predict_file, columns = ('t_id', 'probability'), index = None)
